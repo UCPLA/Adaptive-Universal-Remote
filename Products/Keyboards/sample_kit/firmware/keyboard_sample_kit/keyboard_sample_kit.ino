5c2e2abef672ee66eaa9220c29b60c6e6b059e0d
RubenFixit <git@rubenfixit.com>
2020-08-02 18:12:57 -0600

    Fixed debounce issue by switching rows and columns variables when accessing the global debounce count array. Also assigned keyboard outputs to each key.

--- Products/Keyboards/sample_kit/firmware/keyboard_sample_kit/keyboard_sample_kit.ino
+++ Products/Keyboards/sample_kit/firmware/keyboard_sample_kit/keyboard_sample_kit.ino
@@ -3,6 +3,8 @@
  *****************************************************************************/
 
 
+#include "Keyboard.h"
+
 // Config Variables //
 
 #define NUM_VM_PINS 2
@@ -21,19 +23,29 @@ static const uint8_t vibeMtrPins[NUM_VM_PINS] = {5, 6};
 static const uint8_t btnRowPins[NUM_BTN_ROWS] = {15, 14, 16};
 static const uint8_t btnColPins[NUM_BTN_COLS] = {2, 3, 4, 7, 8, 9};
 
+// The key assigned to each button
+static const char btnKeyCmds[NUM_BTN_ROWS][NUM_BTN_COLS] = {
+  "abcdef",
+  "ghij k",
+  "lmnopq"
+};
+
 // Global Variables //
 
-static int8_t debounce_count[NUM_BTN_COLS][NUM_BTN_ROWS];
+static int8_t debounce_count[NUM_BTN_ROWS][NUM_BTN_COLS];
+static int8_t key_is_down[NUM_BTN_ROWS][NUM_BTN_COLS];
 
 void setup() {
+  
   Serial.begin(9600);
   setupSwitchPins();
   setupVibeMotorPins();
 
-  // Initialize the debounce counter array
-  for (uint8_t i = 0; i < NUM_BTN_COLS; i++) {
-    for (uint8_t j = 0; j < NUM_BTN_ROWS; j++) {
+  // Initialize arrays
+  for (uint8_t i = 0; i < NUM_BTN_ROWS; i++) {
+    for (uint8_t j = 0; j < NUM_BTN_COLS; j++) {
       debounce_count[i][j] = 0;
+      key_is_down[i][j] = false;
     }
   }
 }
@@ -54,21 +66,24 @@ static void scan() {
       if ( debounce_count[currentRow][i] < MAX_DEBOUNCE) {
         debounce_count[currentRow][i]++;
         if ( debounce_count[currentRow][i] == MAX_DEBOUNCE ) {
-          Serial.print("Key Down: "); 
-          Serial.println((currentRow * NUM_BTN_COLS) + i);
+          Serial.print("Key Down: ");
+          Serial.println(btnKeyCmds[currentRow][i]);
+          Keyboard.press(btnKeyCmds[currentRow][i]);
           vibrate_on();
         }
       }
     } else {
-      if ( debounce_count[currentRow][i] == MAX_DEBOUNCE) {
-        Serial.print("Key Up: ");
-        Serial.println((currentRow * NUM_BTN_COLS) + i);
-        debounce_count[currentRow][i] = 0;
-        vibrate_off();
+      if ( debounce_count[currentRow][i] > 0) {
+        debounce_count[currentRow][i]--;
+        if ( debounce_count[currentRow][i] == 0 ) {
+          Serial.print("Key Up: ");
+          Serial.println(btnKeyCmds[currentRow][i]);
+          Keyboard.release(btnKeyCmds[currentRow][i]);
+          vibrate_off();
+        }
       }
     }
   }
-
   // Once done scanning, de-select the switch rows
   digitalWrite(btnRowPins[currentRow], HIGH);
   
