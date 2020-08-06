ce88fb8eb9cdeeb59897835cb2dff5844636c86c
Ruben <ruben@tangibletec.com>
2020-08-06 02:35:29 -0600

    Adding Fontawesome and a couple pages to the demo software. Updated firmware to support capacitive touch.

--- Products/Keyboards/sample_kit/firmware/keyboard_sample_kit/keyboard_sample_kit.ino
+++ Products/Keyboards/sample_kit/firmware/keyboard_sample_kit/keyboard_sample_kit.ino
@@ -1,12 +1,16 @@
-/******************************************************************************
- * 
- *****************************************************************************/
+/*****************************************************************************
+   AUTHOR: Ruben Kackstaetter
+   DATE: July 31, 2020
+   DESCRIPTION: Keyboard firmware for Sparkfun pro micro based off of the code
+   https://learn.sparkfun.com/tutorials/cherry-mx-switch-breakout-hookup-guide
+   This custom keyboard is intended to be used with the Adaptive Universal
+   Remote demo software.
+ ****************************************************************************/
 
 
 #include "Keyboard.h"
 
-// Config Variables //
-
+// Config Variables
 #define NUM_VM_PINS 2
 #define NUM_BTN_COLS 6
 #define NUM_BTN_ROWS 3
@@ -14,14 +18,15 @@
 // Software Debounce
 // Number of high or low senses that trigger a press or release
 #define MAX_DEBOUNCE 6
+#define MAX_CAPT_DEBOUNCE 120
 
 #define VIBERATE_DURRATION_MS 100
 
-// Hardware Setup //
-
+// Hardware Setup
 static const uint8_t vibeMtrPins[NUM_VM_PINS] = {5, 6};
 static const uint8_t btnRowPins[NUM_BTN_ROWS] = {15, 14, 16};
 static const uint8_t btnColPins[NUM_BTN_COLS] = {2, 3, 4, 7, 8, 9};
+static const uint8_t capTouchPin = 20;
 
 // The key assigned to each button
 static const char btnKeyCmds[NUM_BTN_ROWS][NUM_BTN_COLS] = {
@@ -29,43 +34,67 @@ static const char btnKeyCmds[NUM_BTN_ROWS][NUM_BTN_COLS] = {
   "asdf-2",
   "zxcvb3"
 };
+static const char capTouchCmd = btnKeyCmds[0][3];
 
-// Global Variables //
-
+// Global Variables
 static int8_t debounce_count[NUM_BTN_ROWS][NUM_BTN_COLS];
-static int8_t key_is_down[NUM_BTN_ROWS][NUM_BTN_COLS];
+static int8_t capt_debounce_cnt;
 
 void setup() {
-  
+
   Serial.begin(9600);
   setupSwitchPins();
   setupVibeMotorPins();
 
-  // Initialize arrays
+  pinMode(capTouchPin, INPUT);
+
+  // Initialize Globals
+  capt_debounce_cnt = 0;
   for (uint8_t i = 0; i < NUM_BTN_ROWS; i++) {
     for (uint8_t j = 0; j < NUM_BTN_COLS; j++) {
       debounce_count[i][j] = 0;
-      key_is_down[i][j] = false;
     }
   }
 }
 
 void loop() {
-  scan();
+  scan_switch_matrix();
+  scan_cap_touch();
 }
 
-static void scan() {
+static void scan_cap_touch() {
+  if (digitalRead(capTouchPin) == HIGH) {
+    if (capt_debounce_cnt < MAX_CAPT_DEBOUNCE) {
+      capt_debounce_cnt++;
+      if (capt_debounce_cnt == MAX_CAPT_DEBOUNCE) {
+        Serial.print("Key Down: ");
+        Serial.println(capTouchCmd);
+        Keyboard.press(capTouchCmd);
+        vibrate_on();
+      }
+    }
+  } else {
+    if (capt_debounce_cnt > 0) {
+      capt_debounce_cnt = 0;
+      Serial.print("Key Up: ");
+      Serial.println(capTouchCmd);
+      Keyboard.release(capTouchCmd);
+      vibrate_off();
+    }
+  }
+}
+
+static void scan_switch_matrix() {
   static uint8_t currentRow = 0;
   uint8_t i;
 
   digitalWrite(btnRowPins[currentRow], LOW);
 
-  
-  for ( i = 0; i < NUM_BTN_COLS; i++) {
+  for (i = 0; i < NUM_BTN_COLS; i++) {
     if (digitalRead(btnColPins[i]) == LOW) {
-      if ( debounce_count[currentRow][i] < MAX_DEBOUNCE) {
+      if (debounce_count[currentRow][i] < MAX_DEBOUNCE) {
         debounce_count[currentRow][i]++;
-        if ( debounce_count[currentRow][i] == MAX_DEBOUNCE ) {
+        if (debounce_count[currentRow][i] == MAX_DEBOUNCE) {
           Serial.print("Key Down: ");
           Serial.println(btnKeyCmds[currentRow][i]);
           Keyboard.press(btnKeyCmds[currentRow][i]);
@@ -73,9 +102,9 @@ static void scan() {
         }
       }
     } else {
-      if ( debounce_count[currentRow][i] > 0) {
+      if (debounce_count[currentRow][i] > 0) {
         debounce_count[currentRow][i]--;
-        if ( debounce_count[currentRow][i] == 0 ) {
+        if (debounce_count[currentRow][i] == 0 ) {
           Serial.print("Key Up: ");
           Serial.println(btnKeyCmds[currentRow][i]);
           Keyboard.release(btnKeyCmds[currentRow][i]);
@@ -86,7 +115,7 @@ static void scan() {
   }
   // Once done scanning, de-select the switch rows
   digitalWrite(btnRowPins[currentRow], HIGH);
-  
+
   // Increment currentRow, so next time we scan the next row
   currentRow++;
   if (currentRow >= NUM_BTN_ROWS) {
@@ -123,7 +152,8 @@ static void setupSwitchPins() {
     digitalWrite(btnRowPins[i], HIGH);
   }
 
-  // Buttn select columns. Pulled high through resistor. Will be LOW when active
+  // Button select columns
+  // Pulled high with internal resistor, LOW when active.
   for (i = 0; i < NUM_BTN_COLS; i++)  {
     pinMode(btnColPins[i], INPUT_PULLUP);
   }
